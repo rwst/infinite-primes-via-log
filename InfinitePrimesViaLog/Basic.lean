bc6896753bfd9f2321dbf9e513b2eaef41cc7529
Ralf Stephan <ralf@ark.in-berlin.de>
2024-05-12 10:46:40 +0200

    bump

--- InfinitePrimesViaLog/Basic.lean
+++ InfinitePrimesViaLog/Basic.lean
@@ -13,11 +13,25 @@ def S₁ (x : ℝ) : Set ℕ :=
  { n | ∀ p, Nat.Prime p → p ∣ n → p ≤ x }
 
 lemma log_le_harmonic (n : ℕ) (hnx : n ≤ x) (hxn : x < n + 1) :
-    Real.log x ≤ ∑ k in Icc 1 n, (k : ℝ)⁻¹ := by sorry
+    Real.log x ≤ harmonic n := by
+  sorry
 
 lemma H_P4_1 (n : ℕ): (∑ k in Icc 1 n, (k : ℝ)⁻¹) ≤ (∑' m : (S₁ x), (m : ℝ)⁻¹) := by sorry
 
 lemma H_P4_2 : (∑' m : (S₁ x), (m : ℝ)⁻¹) = (∏ p in primesBelow ⌊x⌋.natAbs, (∑' k : ℕ, (p ^ k : ℝ)⁻¹)) := by sorry
+
+lemma H_P4_3 : (∏ p in primesBelow ⌊x⌋₊, (∑' k : ℕ, (p ^ k : ℝ)⁻¹)) ≤ (∏ p in primesBelow ⌊x⌋₊, (p : ℝ) / (p - 1)) := by
+  sorry
+
+lemma H_P4_4a {k p: ℝ} (hk: k > 0) (hp: p ≥ k + 1): p / (p - 1) ≤ (k + 1) / k := by
+  have h_k_nonzero: k ≠ 0 := ne_iff_lt_or_gt.mpr (Or.inr hk)
+  have h_p_pred_pos: p - 1 > 0 := by linarith
+  have h_p_pred_nonzero: p - 1 ≠ 0 := ne_iff_lt_or_gt.mpr (Or.inr h_p_pred_pos)
+  have h₁: p / (p - 1) = 1 + 1 / (p - 1) := by
+    rw [one_add_div h_p_pred_nonzero, sub_add_cancel]
+  rw [← one_add_div h_k_nonzero, h₁, add_le_add_iff_left,
+    one_div_le_one_div h_p_pred_pos hk, @le_sub_iff_add_le]
+  exact hp
 
 theorem monotone_primeCountingReal : Monotone primeCountingReal := by
   intro a b hab
@@ -40,21 +54,22 @@ theorem primeCountingReal_three : primeCountingReal 3 = 2 := by
 theorem primeCountingReal_ge_two (hx : x ≥ 3) : primeCountingReal x ≥ 2 := by
  sorry
 
---lemma H_P4_3 : (∏ p in primesBelow ⌊x⌋.natAbs, (∑' k : ℕ, (p ^ k : ℝ)⁻¹))
---      ≤ (∏ k in Icc 1 (primeCountingReal x), (nth primesBelow k : ℝ) / ((nth primesBelow k) - 1)) := by
---  sorry
+/-theorem nth_prime_ge_add_two : nth Nat.Prime n ≥ n + 2 := by
+  induction' n with n ih
+  . norm_num
+    rw [le_iff_eq_or_lt]
+    exact Or.inl zeroth_prime_eq_two.symm
+  . rw [succ_eq_add_one]
+    rw [ge_iff_le, ← add_le_add_iff_right 1, add_assoc] at ih
+    norm_num at ih
+    simp_arith
+    have h : nth Nat.Prime n + 1 ≤ nth Nat.Prime (n + 1) := by
+      rw [succ_le_iff, nth_lt_nth]
+      exact lt_add_one n
+      sorry
+    exact LE.le.trans ih h -/
 
-lemma H_P4_4a {k p: ℝ} (hk: k > 0) (hp: p ≥ k + 1): p / (p - 1) ≤ (k + 1) / k := by
-  have h_k_nonzero: k ≠ 0 := ne_iff_lt_or_gt.mpr (Or.inr hk)
-  have h_p_pred_pos: p - 1 > 0 := by linarith
-  have h_p_pred_nonzero: p - 1 ≠ 0 := ne_iff_lt_or_gt.mpr (Or.inr h_p_pred_pos)
-  have h₁: p / (p - 1) = 1 + 1 / (p - 1) := by
-    rw [one_add_div h_p_pred_nonzero, sub_add_cancel]
-  rw [← one_add_div h_k_nonzero, h₁, add_le_add_iff_left,
-    one_div_le_one_div h_p_pred_pos hk, @le_sub_iff_add_le]
-  exact hp
-
-lemma H_P4_4 : (∏ k in Icc 1 (primeCountingReal x), (nth Nat.Prime k : ℝ) / ((nth Nat.Prime k) - 1))
+lemma H_P4_4 : (∏ p in primesBelow ⌊x⌋₊, (p : ℝ) / (p - 1))
     ≤ (∏ k in Icc 1 (primeCountingReal x), (k + 1 : ℝ) / k) := by
   --rw [H_P4_4a]
   sorry
@@ -96,4 +111,5 @@ theorem log_le_primeCountingReal_add_one (n : ℕ) (x : ℝ) (hxge : x ≥ n) (h
 
 theorem primeCountingReal_unbounded : Tendsto primeCountingReal atTop atTop := by sorry
 
---theorem infinity_of_primes₄ :
+theorem infinite_setOf_prime : { p | Nat.Prime p }.Infinite :=
+  sorry
